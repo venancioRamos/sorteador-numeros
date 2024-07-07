9edea0631e5646147d3491d7779629d774560f4d
venancio <venancioelias_ramos@hotmail.com>
2024-07-07 15:52:03 -0300

    criação do loop

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 function sortear () {
-    let qnt = document.getElementById('quantidade').value
-    let começo = document.getElementById('de').value
-    let fim = document.getElementById('ate').value
-    alert(`quantidade de numeros ${qnt}`)
+    let qnt = parseInt(document.getElementById('quantidade').value)
+    let começo = parseInt(padocument.getElementById('de').value)
+    let fim = parseInt(document.getElementById('ate').value)
     
 
+    let sorteados = []
+    let numero;
+    for (let i = 0; i < qnt;i++){
+        numero = obterNumeroAleatorio(começo,fim)
+        sorteados.push(numero)
+    }
+    alert(sorteados)
+
+}
+function obterNumeroAleatorio (min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
