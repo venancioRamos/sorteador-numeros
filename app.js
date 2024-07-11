fb89bb81d0f3fdcc65c84f070e2ffcf58034513b
venancio <venancioelias_ramos@hotmail.com>
2024-07-10 21:14:19 -0300

    Revert "criação do loop"
    This reverts commit 9edea0631e5646147d3491d7779629d774560f4d.

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
 function sortear () {
     let qnt = parseInt(document.getElementById('quantidade').value)
-    let começo = parseInt(document.getElementById('de').value)
+    let começo = parseInt(padocument.getElementById('de').value)
     let fim = parseInt(document.getElementById('ate').value)
     
-    let resultado = document.getElementById('resultado')
+
     let sorteados = []
     let numero;
-    if (qnt > (fim - começo + 1)){
-        alert('a quantidade de números sorteados não pode ser maior que o intervalor, favor coloque um intervalo válido')}
-    if (começo> fim){
-        alert('o inicio não pode ser maior que o fim,')
-        }
-    for (let i = 1; i < qnt;i++){
+    for (let i = 0; i < qnt;i++){
         numero = obterNumeroAleatorio(começo,fim)
-        if (sorteados.length == 0){
-        sorteados.push(numero)}
-        while (sorteados.includes(numero)) {
-            numero = obterNumeroAleatorio(começo,fim)
-        }
         sorteados.push(numero)
-
-        resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados} </label>`
-
     }
-   
-    alerarStatusbotao() 
-    }
+    alert(sorteados)
 
-    
-
-
-
+}
 function obterNumeroAleatorio (min,max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
