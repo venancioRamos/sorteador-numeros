3b8c379cbdf5b8602a31e7072e9c5a1a4d839128
venancio <venancioelias_ramos@hotmail.com>
2024-07-10 21:06:56 -0300

    alterações finais

--- app.js
+++ app.js
@@ -6,16 +6,30 @@ function sortear () {
     let resultado = document.getElementById('resultado')
     let sorteados = []
     let numero;
-    for (let i = 0; i < qnt;i++){
+    if (qnt > (fim - começo + 1)){
+        alert('a quantidade de números sorteados não pode ser maior que o intervalor, favor coloque um intervalo válido')}
+    if (começo> fim){
+        alert('o inicio não pode ser maior que o fim,')
+        }
+    for (let i = 1; i < qnt;i++){
         numero = obterNumeroAleatorio(começo,fim)
+        if (sorteados.length == 0){
+        sorteados.push(numero)}
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(começo,fim)
+        }
         sorteados.push(numero)
+
         resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados} </label>`
 
     }
-alerarStatusbotao()
+   
+    alerarStatusbotao() 
+    }
+
     
 
-}
+
 
 function obterNumeroAleatorio (min,max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -28,10 +42,17 @@ function alerarStatusbotao () {
         botao.classList.add('container__botao')
     }
     else {
-        botao.classList.remove('container__botao-desabilitado')
-        botao.classList.add('container__botao')
+        botao.classList.remove('container__botao')
+        botao.classList.add('container__botao-desabilitado')
 
 
     }
 
+}
+function reiniciar () {
+    document.getElementById('quantidade').value =''
+    document.getElementById('de').value =''
+    document.getElementById('ate').value=''
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+    alerarStatusbotao()
 }
