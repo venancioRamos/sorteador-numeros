90f907453f13c2453c312bfdb9ba5ae141ed9cf8
venancio <venancioelias_ramos@hotmail.com>
2024-07-10 21:16:51 -0300

    Revert "alterações finais"
    This reverts commit 3b8c379cbdf5b8602a31e7072e9c5a1a4d839128.

--- app.js
+++ app.js
@@ -6,13 +6,26 @@ function sortear () {
 
     let sorteados = []
     let numero;
-    for (let i = 0; i < qnt;i++){
+    if (qnt > (fim - começo + 1)){
+        alert('a quantidade de números sorteados não pode ser maior que o intervalor, favor coloque um intervalo válido')}
+    if (começo> fim){
+        alert('o inicio não pode ser maior que o fim,')
+        }
+    for (let i = 1; i < qnt;i++){
         numero = obterNumeroAleatorio(começo,fim)
         sorteados.push(numero)
-    }
-    alert(sorteados)
 
-}
+        resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados} </label>`
+
+    }
+   
+    alerarStatusbotao() 
+    }
+
+    
+
+
+
 function obterNumeroAleatorio (min,max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -24,17 +37,10 @@ function alerarStatusbotao () {
         botao.classList.add('container__botao')
     }
     else {
-        botao.classList.remove('container__botao')
-        botao.classList.add('container__botao-desabilitado')
+        botao.classList.remove('container__botao-desabilitado')
+        botao.classList.add('container__botao')
 
 
     }
 
-}
-function reiniciar () {
-    document.getElementById('quantidade').value =''
-    document.getElementById('de').value =''
-    document.getElementById('ate').value=''
-    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
-    alerarStatusbotao()
 }
